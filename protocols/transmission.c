b2e1696118adde8398c03dca09a7a061938acf47
xiangjs <951425301@qq.com>
2020-08-18 14:17:46 +0800

    添加多个关于网络收发的函数

--- protocols/transmission.c
+++ protocols/transmission.c
@@ -14,8 +14,8 @@
 #include "protocols.h"
 #include "../Image/ImagePyrTree.h"
 
-#define PORT 12345
-#define MESSAGE_LEN 10240
+#define PORT 1234
+#define MESSAGE_LEN 1024 * 1024
 
 static int init_sever_socket(void)
 {
@@ -54,6 +54,82 @@ static int force_read(int fd, char *buf, size_t len)
     return len;
 }
 
+static int recv_net_requst_image(int fd, image_requst_protocol *image_requst_message)
+{
+    char message[MESSAGE_LEN];
+    int n;
+    if ((n = force_read(fd, message, IMAGE_REQUST_HEAD_SIZE)) != IMAGE_REQUST_HEAD_SIZE)
+        return -1;
+    if (net_to_image_requst(message, n, image_requst_message) < 0)
+        return -1;
+    return n;
+}
+
+static int send_net_response_image(int fd, image_response_protocol *image_response_message)
+{
+    enum label_type label = RESPONSE_IMAGE;
+    char message[MESSAGE_LEN];
+    int n_len = 0;
+    int n;
+    if ((n = putLabelToNet(label, message, MESSAGE_LEN)) < 0)
+        return -1;
+    n_len += n;
+    if((n = image_response_head_to_net(image_response_message, message + n_len, MESSAGE_LEN - n_len)) < 0)
+        return -1;
+    n_len += n;
+    if (image_response_message->head.len != 0) {
+        memcpy(message + n_len, image_response_message->data, image_response_message->head.len);
+        n_len += image_response_message->head.len;
+    }
+    if (write(fd, message, n_len) < 0)
+        return -1;
+    return n_len;
+}
+
+static int send_net_requst_image(int fd, image_requst_protocol *image_requst_message)
+{
+    char message[MESSAGE_LEN];
+    enum label_type label = REQUST_IMAGE;
+    int n_len = 0;
+    int n;
+    if ((n = putLabelToNet(label, message, MESSAGE_LEN)) < 0)
+        return -1;
+    n_len += n;
+    if ((n = image_requst_to_net(image_requst_message, message + n_len, MESSAGE_LEN - n_len)) < 0)
+        return -1;
+    n_len += n;
+    if (write(fd, message, n_len) != n_len)
+        return -1;
+    return n_len;
+}
+
+static int recv_net_response_image(int fd, image_response_protocol *image_response_message)
+{
+    char message[MESSAGE_LEN];
+    int n_len = 0;
+    int n;
+
+    if ((n = force_read(fd, message, IMAGE_RESPONSE_HEAD_SIZE)) < 0) {
+        return -1;
+    }
+    net_to_image_head_response(message, n, image_response_message);
+    n_len += n;
+    return n_len;
+}
+
+static enum label_type recv_net_label(int fd)
+{
+    char message[LABEL_SIZE];
+    enum label_type label;
+    int n;
+    if ((n = force_read(fd, message, LABEL_SIZE)) != LABEL_SIZE) {
+        return -1;
+    }
+    if (getLabelFromNet(message, n, &label) < 0)
+        return -1;
+    return label;
+}
+
 void sever_transmission_proccess(int sockfd)
 {
     int client_fd = init_sever_socket();
@@ -63,7 +139,7 @@ void sever_transmission_proccess(int sockfd)
     image_response_protocol image_response_message;
     struct encode_response_protocol encode_response_message;
     enum label_type label;
-    int trans_len;
+    //int trans_len;
 
     char *message = malloc(MESSAGE_LEN);
     if (!message)
@@ -71,15 +147,19 @@ void sever_transmission_proccess(int sockfd)
     //图像传输
     while (true)
     {
-        if ((trans_len = force_read(client_fd, message, LABEL_SIZE)) != LABEL_SIZE)
+        if ((label = recv_net_label(client_fd)) < 0)
             break;
-        getLabelFromNet(message, trans_len, &label);
+        /*if ((trans_len = force_read(client_fd, message, LABEL_SIZE)) != LABEL_SIZE)
+            break;
+        getLabelFromNet(message, trans_len, &label);*/
 
         if (label != REQUST_IMAGE)
             continue;
-        if ((trans_len = force_read(client_fd, message, IMAGE_REQUST_HEAD_SIZE)) != IMAGE_REQUST_HEAD_SIZE)
+        if (recv_net_requst_image(client_fd, &image_requst_message) < 0)
             break;
-        net_to_image_requst(message, trans_len, &image_requst_message);
+        /*if ((trans_len = force_read(client_fd, message, IMAGE_REQUST_HEAD_SIZE)) != IMAGE_REQUST_HEAD_SIZE)
+            break;
+        net_to_image_requst(message, trans_len, &image_requst_message);*/
 
         program_message_header.protocol_label = REQUST_ENCODE_IMAGE;
         program_message_header.requst_encode.seq = image_requst_message.seq;
@@ -137,7 +217,9 @@ void sever_transmission_proccess(int sockfd)
                     //printf("sever_transmission_proccess NEW %d %d\n", image_response_message.head.type, NEW);
                 }*/
 
-                label = RESPONSE_IMAGE;
+                if (send_net_response_image(client_fd, &image_response_message) < 0)
+                    return;
+                /*label = RESPONSE_IMAGE;
                 int n_len = 0;
                 int n;
                 if ((n = putLabelToNet(label, message, MESSAGE_LEN)) < 0)
@@ -153,7 +235,10 @@ void sever_transmission_proccess(int sockfd)
                 }
                 //static int count = 0;
                 //printf("%d\n", n_len);
-                write(client_fd, message, n_len);
+                write(client_fd, message, n_len);*/
+                if (image_response_message.head.len != 0) {
+                    shareFree(pyramids_array[index]->image.data);
+                }
                 shareFree(pyramids_array[index]);
 
                 /*
@@ -173,7 +258,7 @@ void client_transmission_proccess(int sockfd)
 {
     int sever_fd = init_client_socket();
     //MeshHead *mesh_head;
-    Rect mesh_size;
+    //Rect mesh_size;
     Rect mesh_num_size;
     struct program_protocol program_message_header;
     image_requst_protocol image_requst_message;
@@ -197,8 +282,12 @@ void client_transmission_proccess(int sockfd)
         //mesh_head = getShareMemory(program_message_header.requst_decode.mesh_head_key);
         //mesh_size = program_message_header.requst_decode.mesh_size;
         mesh_num_size = program_message_header.requst_decode.mesh_num_size;
+
         image_requst_message.seq = program_message_header.requst_decode.seq;
-        label = REQUST_IMAGE;
+        if (send_net_requst_image(sever_fd, &image_requst_message) < 0)
+            break;
+
+        /*label = REQUST_IMAGE;
         int n_len = 0;
         int n;
         if ((n = putLabelToNet(label, message, MESSAGE_LEN)) < 0)
@@ -208,31 +297,39 @@ void client_transmission_proccess(int sockfd)
             return;
         n_len += n;
         if (write(sever_fd, message, n_len) != n_len)
-            break;
+            break;*/
 
         //Mesh *mesh = mesh_head->mesh;
         int num_size = RECT_LENGTH(mesh_num_size);
         flow_statistics = 0;
         for (int i = 0; i < num_size; i++) {
-            if ((trans_len = force_read(sever_fd, message, LABEL_SIZE)) != LABEL_SIZE) {
+
+            if ((label = recv_net_label(sever_fd)) < 0)
+                return;
+
+            flow_statistics += LABEL_SIZE;
+
+            /*if ((trans_len = force_read(sever_fd, message, LABEL_SIZE)) != LABEL_SIZE) {
                 return;
             }
-
             flow_statistics += trans_len;
 
-            getLabelFromNet(message, trans_len, &label);
+            getLabelFromNet(message, trans_len, &label);*/
             if (label != RESPONSE_IMAGE) {
                 i--;
                 continue;
             }
 
-            if ((trans_len = force_read(sever_fd, message, IMAGE_RESPONSE_HEAD_SIZE)) < 0) {
+            if ((trans_len = recv_net_response_image(sever_fd, &image_response_message) < 0))
+                return;
+            flow_statistics += trans_len;
+/*            if ((trans_len = force_read(sever_fd, message, IMAGE_RESPONSE_HEAD_SIZE)) < 0) {
                 return;
             }
 
             flow_statistics += trans_len;
 
-            net_to_image_head_response(message, trans_len, &image_response_message);
+            net_to_image_head_response(message, trans_len, &image_response_message);*/
             if (image_response_message.head.seq != program_message_header.requst_decode.seq) {
                 i--;
                 continue;
